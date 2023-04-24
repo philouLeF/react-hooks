351b5f64e7c60ae71d7ab16727bd4ef84cd46eb6
philouLeF <delcroixphilippe73@gmail.com>
2023-04-24 23:44:54 +0200

    useref

--- src/exercise/3-use-ref/03.jsx
+++ src/exercise/3-use-ref/03.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable no-unused-vars */ // 🦁 Enlève cette ligne
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const useDebounce = (callback, time) => {
-  // 🦁 Remplace cette variable par un `useRef`
-  // 💡 timeout correspond à la référence de notre timeout.
-  //   Quand tu fais un setTimeout, il return une valeur que
-  //   tu peux clear afin de l'annuler. https://developer.mozilla.org/fr/docs/Web/API/setTimeout#valeur_de_retour
-  const timeout = null;
+  const timeoutRef = useRef(null);
 
   const onDebounce = (...args) => {
-    // 🦁 Annule le timeout https://developer.mozilla.org/en-US/docs/Web/API/clearTimeout
-    // ℹ️ Cette fonction sera appelée à chaque fois que l'user tape un caractère, on veut donc clear
-    //    le dernier timeout pour relancer un nouveau timeout.
-    // 🦁 Crée un nouveau timeout https://developer.mozilla.org/en-US/docs/Web/API/setTimeout
-    //    a la fin il doit appeler la callback avec les arguments et le temps est défini par le paramètre `time`
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, time);
   };
 
   return onDebounce;
+};
+
+const useRenderCount = () => {
+  const renderCountRef = useRef(0);
+  useEffect(() => {
+    renderCountRef.current += 1;
+  });
+
+  return renderCountRef;
 };
 
 const fetchAgeByName = (name) => {
@@ -25,18 +29,19 @@ const fetchAgeByName = (name) => {
 
 const App = () => {
   const [result, setResult] = useState(null);
+  const inputRef = useRef(null);
+  const renderCountRef = useRenderCount();
 
-  // 🦁 Wrap la function `onSearch` dans le hooks useDebounce
-  // 💡 const onSearch = useDebounce((value) => {...}, 500);
-  const onSearch = (value) => {
-    fetchAgeByName(value).then((data) => {
+  const onSearch = useDebounce((value) => {
+    fetchAgeByName(inputRef.current.value).then((data) => {
       setResult(data);
     });
-  };
+  }, 500);
 
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         placeholder="Search bar"
         onChange={(event) => {
@@ -49,6 +54,7 @@ const App = () => {
           <b>{result.count}</b> people with this name.
         </div>
       ) : null}
+      <p>The component render {renderCountRef.current} times</p>
     </div>
   );
 };
