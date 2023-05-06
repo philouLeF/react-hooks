c3a00ff4621f0868e38cbee181d9b38d4400fd77
philouLeF <delcroixphilippe73@gmail.com>
2023-05-06 23:23:12 +0200

    useContext fin

--- src/exercise/4-use-context/04.jsx
+++ src/exercise/4-use-context/04.jsx
@@ -1,10 +1,28 @@
 import clsx from 'clsx';
-import { useReducer, useState, createContext, useContext } from 'react';
+import {
+  useReducer,
+  useState,
+  createContext,
+  useContext,
+  useMemo,
+} from 'react';
 
 const ThemeContext = createContext(null);
 
 const useThemeContext = () => {
   const context = useContext(ThemeContext);
+
+  if (context === null) {
+    throw new Error('useThemeContext must be used inside a ThemeProvider');
+  }
+
+  return context;
+};
+
+const ThemeDispatchContext = createContext(null);
+
+const useThemeDispatchContext = () => {
+  const context = useContext(ThemeDispatchContext);
 
   if (context === null) {
     throw new Error('useThemeContext must be used inside a ThemeProvider');
@@ -22,12 +40,22 @@ const ThemeProvider = ({ children }) => {
   const setLight = () => setTheme('light');
 
   const isDark = theme === 'dark';
-  const islight = theme === 'light';
+  const isLight = theme === 'light';
 
-  const values = { theme, toggle, setDark, setLight, isDark, islight };
+  const values = useMemo(() => {
+    return { theme, isDark, isLight };
+  }, [isLight, isDark, theme]);
+
+  const dispatchValues = useMemo(() => {
+    return { toggle, setDark, setLight };
+  }, []);
 
   return (
-    <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={values}>
+      <ThemeDispatchContext.Provider value={dispatchValues}>
+        {children}
+      </ThemeDispatchContext.Provider>
+    </ThemeContext.Provider>
   );
 };
 
@@ -42,19 +70,20 @@ const ThemedLayout = ({ children }) => {
 };
 
 const ForceLightMode = () => {
-  const { setLight } = useThemeContext();
+  const { setLight } = useThemeDispatchContext();
 
   return <button onClick={() => setLight()}>Force light</button>;
 };
 
 const ForceDarkMode = () => {
-  const { setDark } = useThemeContext();
+  const { setDark } = useThemeDispatchContext();
 
   return <button onClick={() => setDark()}>Force dark</button>;
 };
 
 const ToggleMode = () => {
-  const { toggle, isDark } = useThemeContext();
+  const { isDark } = useThemeContext();
+  const { toggle } = useThemeDispatchContext();
 
   return <button onClick={toggle}>{isDark ? '🌞' : '🌙'}</button>;
 };
