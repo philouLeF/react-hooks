0f8120f835482d0e49ee21a4ba64f78891cc90d4
philouLeF <delcroixphilippe73@gmail.com>
2023-05-07 08:34:35 +0200

    useReducer le goat

--- src/exercise/5-use-reducer/05.jsx
+++ src/exercise/5-use-reducer/05.jsx
@@ -1,18 +1,58 @@
 import { useReducer } from 'react';
 
-const reducer = (value, action) => {
-  // 🦁 Ici il faut que tu return la valeur incrémenté de 1
+const REDUCER_ACTIONS = {
+  PLUS: 'plus',
+  MINUS: 'minus',
+  RESET: 'reset',
+};
+
+const reducer = (value, { action, operation }) => {
+  switch (action) {
+    case REDUCER_ACTIONS.PLUS:
+      return value + operation;
+    case REDUCER_ACTIONS.MINUS:
+      return value - operation;
+    case REDUCER_ACTIONS.RESET:
+      return 0;
+    default:
+      throw new Error('pas ça');
+  }
 };
 
 const Counter = () => {
   // 🦁 Remplace ceci par un useReducer avec `reducer` en paramètre et `0` en initialState
-  let count = 0;
+  const [count, dispatch] = useReducer(reducer, 0);
 
   return (
     <div>
+      <button
+        onClick={() =>
+          dispatch({ action: REDUCER_ACTIONS.MINUS, operation: 5 })
+        }
+      >
+        -5
+      </button>
+      <button
+        onClick={() =>
+          dispatch({ action: REDUCER_ACTIONS.MINUS, operation: 1 })
+        }
+      >
+        -1
+      </button>
       <button>{count}</button>
-      {/* 🦁 Ajoute un `onClick` qui appel la fonction dispatch de notre reducer */}
-      <button>+</button>
+      <button
+        onClick={() => dispatch({ action: REDUCER_ACTIONS.PLUS, operation: 1 })}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => dispatch({ action: REDUCER_ACTIONS.PLUS, operation: 5 })}
+      >
+        +5
+      </button>
+      <button onClick={() => dispatch({ action: REDUCER_ACTIONS.RESET })}>
+        Reset
+      </button>
     </div>
   );
 };
